900750c991350eb6725f48472c9d7a49567d815c
lordkiro <57450671+lordkiro@users.noreply.github.com>
2021-01-24 12:49:13 +0100

    Add files via upload

---
+++ TP1/src/control/Voler.java
@@ -0,0 +1,54 @@
+/**
+ * Cette classe gere la descente automatique de l'ovale. 
+ */
+
+package control;
+
+import model.Etat;
+import view.Affichage;
+
+public class Voler extends Thread{
+	
+	/** Temps de latence entre deux descentes en millisecondes
+	 * @warning modifier coeffChute si on modifie*/
+	public static int t = 50;
+	
+	/** L'etat que l'on va modifier */
+	public Etat e;
+	
+	/** L'affichage que l'on doit mettre a jour avec moveDown*/
+	public Affichage a;
+	
+	/**
+	 * Constructeur Voler
+	 * Affecte l'etat et l'affichage passe en parametre aux
+	 * constantes de la classe.
+	 * 
+	 * @param Etat etat, l'etat que l'on modifiera
+	 * @param Affiachage aff, l'affichage auquel 
+	 * on demandera des mises a jour
+	 */
+	public Voler(Etat etat, Affichage aff) {
+		this.e = etat;
+		this.a = aff;
+	}
+	
+	/**
+	 * Methode run
+	 * Boucle infini qui va appeler moveDown de la classe Etat
+	 * toutes les t millisecondes. 
+	 */
+	@Override
+	public void run() {
+		while(true) {
+		      try {
+		    	  e.moveDown(Affichage.yS);
+		    	  a.revalidate(); //On force le dessin pour eviter les ralentissements
+		  		  a.repaint();
+		    	  Thread.sleep(t); 
+		    	  }
+		      catch (Exception e) { e.printStackTrace(); 
+		      }
+		}
+	}
+}
