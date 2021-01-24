1758721db4c150b07a1aea04b8b60770257ad420
lordkiro <57450671+lordkiro@users.noreply.github.com>
2021-01-24 23:37:20 +0100

    Update Main.java
    Mise à jour du main

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -1,7 +1,11 @@
+
 import javax.swing.JFrame;
 
+import control.Avancer;
 import control.Control;
+import control.Voler;
 import model.Etat;
+import model.Parcours;
 import view.Affichage;
 
 public class Main {
@@ -9,12 +13,19 @@ public class Main {
 	public static void main(String[] args) {
 		Etat Modele = new Etat();
 		Affichage Vue = new Affichage(Modele);
+		Voler Vol = new Voler(Modele, Vue);
 		Control Controleur = new Control(Modele, Vue);
+		Parcours Parcours = new Parcours(Modele);
+		Avancer Progression = new Avancer(Parcours, Vue);
+		Vue.setParcours(Parcours);
 		
 		JFrame test = new JFrame("TP1");
 		test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		test.add(Vue);
 		test.pack();
 		test.setVisible(true);
+		
+		Vol.start();
+		Progression.start();
 	}
 }
