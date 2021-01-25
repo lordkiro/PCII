e64f705a64abce60182aac7ee24560d51f78093c
lordkiro <57450671+lordkiro@users.noreply.github.com>
2021-01-25 15:37:59 +0100

    Update Avancer.java
    Ajout : terminate
    modif de run

--- TP1/src/control/Avancer.java
+++ TP1/src/control/Avancer.java
@@ -4,19 +4,32 @@
 
 package control;
 
+import model.Etat;
 import model.Parcours;
 import view.Affichage;
 
 public class Avancer extends Thread{
-	/** Temps de latence entre deux décallages en millisecondes
+	/** Temps de latence entre deux dÃ©callages en millisecondes
 	 * @warning modifier coeffChute si on modifie*/
-	public static int t = 50;
+	public static int t = 200;
 	
 	/** Le parcours que l'on va faire defiler */
 	public Parcours p;
 	
 	/** L'affichage que l'on doit mettre a jour*/
 	public Affichage a;
+	
+	
+	public Etat e;
+	
+	
+	public Control c;
+	
+	
+	public boolean running;
+	
+	
+	public Voler vol;
 	
 	/**
 	 * Constructeur Avancer
@@ -27,9 +40,13 @@ public class Avancer extends Thread{
 	 * @param Affiachage aff, l'affichage auquel 
 	 * on demandera des mises a jour
 	 */
-	public Avancer(Parcours parc, Affichage aff) {
+	public Avancer(Etat etat, Parcours parc, Affichage aff, Control c, Voler vol) {
+		this.e = etat;
 		this.p = parc;
 		this.a = aff;
+		this.c = c;
+		this.vol = vol;
+		vol.av = this;
 	}
 	
 	/**
@@ -39,15 +56,28 @@ public class Avancer extends Thread{
 	 */
 	@Override
 	public void run() {
-		while(true) {
+		running = true;
+		while(running) {
 		      try {
 		    	  p.setPos();
 		    	  a.revalidate(); //On force le dessin pour eviter les ralentissements
 		  		  a.repaint();
+		  		  if(e.testPerdu()) {
+		  			  terminate();
+		  		  }
 		    	  Thread.sleep(t); 
 		    	  }
 		      catch (Exception e) { e.printStackTrace(); 
 		      }
+		}
+	}
+	
+	public void terminate() {
+		if(running) {
+			running = false;
+			c.running = false;
+			vol.terminate();
+			a.endingscreen();
 		}
 	}
 }
