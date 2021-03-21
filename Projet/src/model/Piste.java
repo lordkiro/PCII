e296ecf47898bf5ae5f04db388ff799c7eaea58d
lordkiro <57450671+lordkiro@users.noreply.github.com>
2021-03-21 20:33:20 +0100

    Update Piste.java

--- Projet/src/model/Piste.java
+++ Projet/src/model/Piste.java
@@ -27,7 +27,7 @@ public class Piste  extends ArrayList<Point>{
 		 * @warning modifier coeffChute si on modifie */
 		public int increPos = 2;
 
-		/**Le nombre de points visibles en mÍme temps sur la piste*/
+		/**Le nombre de points visibles en m√™me temps sur la piste*/
 		public static int nbPVisibles = 5;
 		
 		/**La hauteur de l'horizon dans la fenetre*/
@@ -35,6 +35,27 @@ public class Piste  extends ArrayList<Point>{
 		
 		/** Ecart entre deux points */
 		public static int ecart = (xS-horizon)/nbPVisibles;
+		
+		/**Distance max en x entre deux points*/
+		public static int maxDiffX = 150;
+		
+		/** Interval minimum entre deux obstacles*/
+		public static int intObs;
+		
+		/** Intervalle entre deux points de controles. En nombre de segments*/
+		public static int interPC;
+		
+		/** Niveau de difficult√©e */
+		public double diff;
+		
+		/** Facteur de difficult√© max*/
+		public double maxdiff = 3.5;
+		
+		/** Compte de segment cr√©√©s sans PC*/
+		private int decomptePC;
+		
+		/** Liste des numeros de segments des PC*/
+		public ArrayList<Integer> pointsControl;
 		
 		/**
 		 * Constructeur Piste
@@ -48,15 +69,14 @@ public class Piste  extends ArrayList<Point>{
 			e = etat;
 			
 			int i = 0;	//i representera l'indice du dernier point ajoute a la piste
-			int x = etat.xC + etat.w/4; //On ferra commencÈ la piste au centre de l'ovale
+			int x = etat.xC + etat.w/4; //On ferra commenc√© la piste au centre de l'ovale
 			int y = etat.yC+etat.h/2;			//dans un soucis de faisabilite de la piste
 			this.add(new Point(x, y));
 			while(i < nbPVisibles) {	//On cree des points jusqu'a ce qu'on sorte de la fenetre
 				x = rand.nextInt(xS);
 				y -= ecart;	
 				Point p = new Point(x, y);
-				if( x > etat.h/2 && x < xS - etat.h*3/2){ //Si un point est valide  on l'ajoute, sinon on en cree un autre
-					//On veut aussi que le point que l'on crÈe soit ‡ une bonne distance des bords infÈrieur et supÈrieur de la fenÍtre, afin de faciliter la phase de jeu.
+				if( x > etat.h/2 && x < xS - etat.h*3/2 && suiteValide(this.get(i), p)){ //Si un point est valide  on l'ajoute, sinon on en cree un autre
 					this.add(p);
 					i++;
 				}
@@ -66,6 +86,11 @@ public class Piste  extends ArrayList<Point>{
 			}
 			e.setPiste(this);//on ajoute la piste a l'etat du jeu
 			position = 0; //initialisation du score a 0
+			intObs = 1000;
+			interPC = 7;
+			decomptePC = interPC - nbPVisibles;
+			pointsControl = new ArrayList<Integer>();
+			diff = 1;
 		}
 
 		/**
@@ -82,7 +107,7 @@ public class Piste  extends ArrayList<Point>{
 				Point pi = get(i);
 				if(estVisible(new Point(pi.x - e.decX, pi.y + position))) { //Si un point est visible apres l'avancee, on l'ajoute a la liste
 					Point pr = new Point(pi.x - e.decX, pi.y + position);//On ajoute le decX (decallage sur l'axe des X par rapport au centre de la piste) pour decaler l'afficahge des points
-					Point pr2 = new Point(pi.x+e.w/2 - e.decX, pi.y + position); //Ce qui nous permet d'avoir le point au centre de la fenetre et le reste du monde qui bouge
+					Point pr2 = new Point(pi.x+e.w - e.decX, pi.y + position); //Ce qui nous permet d'avoir le point au centre de la fenetre et le reste du monde qui bouge
 					res.add(pr);
 					res.add(pr2);//La piste est composee de deux lignes paralleles, on represente donc deux fois les lignes avec un decalage (d'ou le +e.w/2 plus haut)
 					}
@@ -91,7 +116,7 @@ public class Piste  extends ArrayList<Point>{
 				precedent est visible, on a besoin du point hors de la fenetre pour creer le segment qui sera partiellement visible. On l'ajoute donc
 				a la liste. De plus si ce point est bientot visible, on ajoute un autre point derriere lui*/
 					Point pr = new Point(pi.x- e.decX, pi.y + position);
-					Point pr2 = new Point(pi.x+e.w/2- e.decX, pi.y + position);
+					Point pr2 = new Point(pi.x+e.w- e.decX, pi.y + position);
 					res.add(pr);
 					res.add(pr2);
 				}
@@ -99,19 +124,42 @@ public class Piste  extends ArrayList<Point>{
 						&& estVisible(new Point(get(i-1).x - e.decX, get(i-1).y + position))) { /*Si un point est hors
 				 de la fenetre a gauche et que le suivant est visible, on a besoin du point pour tracer le segment qui sera partiellement visible. On l'ajoute donc a la liste*/
 					Point pr = new Point(pi.x- e.decX, horizon);
-					Point pr2 = new Point(pi.x +e.w/2- e.decX, horizon);
+					Point pr2 = new Point(pi.x +e.w- e.decX, horizon);
 					res.add(pr);
 					res.add(pr2);
 					if(pi.y + e.vitesse*1.5 + position > horizon && i == size()-1) { //on met increPos*1.5 pour regarder un peu plus loin que la prochaine etape
 						ajoutePoint();
+						if(this.decomptePC - 1 == 0) {
+							pointsControl.add(i+1);
+							decomptePC = (int) Math.round(interPC * diff); /**TODO modifier doc*/
+							diff += 0.2;
+						}else {
+							decomptePC -= 1;
+						}
 					}
 					return res.toArray(new Point[res.size()]); /* Pas besoin de s'embeter a aller plus loin puisqu'on affichera 
-					pas les points stockÈs plus tard si il y en a.*/
+					pas les points stock√©s plus tard si il y en a.*/
 				}
 			}
 			return res.toArray(new Point[res.size()]);
 		}
 		
+		/**
+		 * Fonction getPC
+		 * 
+		 * @return Point[] Le point le plus a gauche des PC visibles
+		 */
+		
+		public Point[] getPC() {
+			ArrayList<Point> res = new ArrayList<Point>();
+			for(int i : this.pointsControl) {
+				Point p = this.get(i);
+				if(estVisible(new Point(p.x - e.decX, p.y + position))) {
+					res.add(new Point(p.x-e.w/2-e.decX, p.y+position));
+				}
+			}
+			return res.toArray(new Point[res.size()]);
+		}
 		/**
 		 * Fonction getPos
 		 * Renvoie le score (position par rapport au depart) sur la piste actuel
@@ -127,7 +175,7 @@ public class Piste  extends ArrayList<Point>{
 		 * incremente la position (score) de la constante increPos
 		 */
 		public void setPos() {
-			position += e.vitesse;
+			position += Math.round(e.vitesse);
 		}
 		
 		/**
@@ -153,11 +201,23 @@ public class Piste  extends ArrayList<Point>{
 			while (flag==false) { // tant que la suite de points n'est pas valide, on cree un nouveau point
 				int x = rand.nextInt(yS);
 				p = new Point(x, y);
-				if( x > e.h/2 && x < xS - e.h*3/2){ //Si la suite du dernier point dans le tableau avec le point que l'on cree est valide, on sort de la boucle
-					//On veut aussi que le point que l'on crÈe soit ‡ une bonne distance des bords infÈrieur et supÈrieur de la fenÍtre, afin de faciliter la phase de jeu.
+				if( x > e.h/2 && x < xS - e.h*3/2 && suiteValide(this.get(i), p)){ //Si la suite du dernier point dans le tableau avec le point que l'on cree est valide, on sort de la boucle
+					//On veut aussi que le point que l'on cr√©e soit √† une bonne distance des bords inf√©rieur et sup√©rieur de la fen√™tre, afin de faciliter la phase de jeu.
 					flag = true;
 				}
 			}
 			add(p);
+		}
+		
+		/** 
+		 * Fonction suiteValide
+		 * Renvoie si l'abscisse du point suivant est assez proche de l'abscisse du point precedent.
+		 * 
+		 * @param Point p1, le point precedent
+		 * @param Point p2, le point que l'on test
+		 * @return boolean le resultat 
+		 */
+		public boolean suiteValide(Point p1, Point p2) {
+			return (Math.abs(p1.x - p2.x) < maxDiffX);
 		}
 }
